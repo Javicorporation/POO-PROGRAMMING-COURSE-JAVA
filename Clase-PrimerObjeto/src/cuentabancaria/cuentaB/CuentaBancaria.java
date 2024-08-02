3c028d174ab2f48ef63828da53e4df6a61f5d96e
Javicorporation <krchipullajavier@gmail.com>
2024-08-02 16:23:32 -0500

    - Implementación de modificadores de acceso. - constructor get y set

--- Clase-PrimerObjeto/src/cuentabancaria/cuentaB/CuentaBancaria.java
+++ Clase-PrimerObjeto/src/cuentabancaria/cuentaB/CuentaBancaria.java
@@ -2,9 +2,56 @@ package cuentabancaria.cuentaB;
 
 public class CuentaBancaria {
 
-    String CBU;
-    String alias;
-    double saldo;
+    private String CBU;
+    private String alias;
+    private double saldo;
+    private String moneda;
+    private String tipo;
+
+    // constructor
+    public CuentaBancaria(String alias, double saldo) {
+        this.CBU = generarCBU();
+        setAlias(alias);
+        this.saldo = saldo;
+        setMoneda(moneda);
+        setTipo(tipo);
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String generarCBU(){
+        return "r56rt3748";
+    }
 
     public void mostrarDatos(){
         System.out.println(CBU+" - "+alias+" - "+saldo);
--- Clase-PrimerObjeto/src/cuentabancaria/cuentaB/Principal.java
+++ Clase-PrimerObjeto/src/cuentabancaria/cuentaB/Principal.java
@@ -2,21 +2,11 @@ package cuentabancaria.cuentaB;
 
 public class Principal {
     public static void main(String[] args) {
-        CuentaBancaria cuentaBanc = new CuentaBancaria();
-        cuentaBanc.mostrarDatos();
-        cuentaBanc.CBU = "fdhfj";
-        cuentaBanc.alias = "sjfhjd";
+        CuentaBancaria cuentaBanc = new CuentaBancaria("ereer",200);
         cuentaBanc.mostrarDatos();
 
-        System.out.println("el saldo es: "+cuentaBanc.mostrarSaldo());
-        cuentaBanc.depositar(100);
-        System.out.println("el saldo es: "+cuentaBanc.mostrarSaldo());
 
-
-        System.out.println(cuentaBanc.saldoDisponible(100));
-        System.out.println(cuentaBanc.saldoDisponible(3000));
-        cuentaBanc.retira(200);
-        System.out.println("el saldo es: "+cuentaBanc.mostrarSaldo());
+        cuentaBanc.mostrarDatos();
 
 
 
