253cb441c4d888861c92cb152595fa22bae6a71c
Javicorporation <krchipullajavier@gmail.com>
2024-08-20 18:33:22 -0500

    Uso de enums y sus métodos como obtener un string o convertirlo a aún array

--- muestra de eneums/src/com/enums/Test/Test.java
+++ muestra de eneums/src/com/enums/Test/Test.java
@@ -7,17 +7,29 @@ import com.enums.clases.Persona;
 public class Test {
     public static void main(String[] args) {
 
-        Empresa e = new Empresa();
+//        Empresa e = new Empresa();
+////
+////        e.agregarPersona(new Persona("Pepe", "Fulano", NivelEstudio.SECUNDARIO));
+////        e.agregarPersona(new Persona("Maria", "Sultana", NivelEstudio.SECUNDARIO));
+////        e.agregarPersona(new Persona("Luis", "Mengano", NivelEstudio.TERCIARIO));
+////        e.agregarPersona(new Persona("Lito", "García", NivelEstudio.SECUNDARIO));
+////        e.agregarPersona(new Persona("Arón", "Quilez", NivelEstudio.PRIMARIO));
+////        e.agregarPersona(new Persona("Rita", "Perez", NivelEstudio.UNIVERSITARIO));
+////        e.agregarPersona(new Persona("Alex", "Gomez", NivelEstudio.SECUNDARIO));
+////        e.agregarPersona(new Persona("Sara", "Jerez", NivelEstudio.SECUNDARIO));
+////
+////        e.mostrarPorNivel(NivelEstudio.UNIVERSITARIO);
 
-        e.agregarPersona(new Persona("Pepe", "Fulano", NivelEstudio.SECUNDARIO));
-        e.agregarPersona(new Persona("Maria", "Sultana", NivelEstudio.SECUNDARIO));
-        e.agregarPersona(new Persona("Luis", "Mengano", NivelEstudio.TERCIARIO));
-        e.agregarPersona(new Persona("Lito", "García", NivelEstudio.SECUNDARIO));
-        e.agregarPersona(new Persona("Arón", "Quilez", NivelEstudio.PRIMARIO));
-        e.agregarPersona(new Persona("Rita", "Perez", NivelEstudio.UNIVERSITARIO));
-        e.agregarPersona(new Persona("Alex", "Gomez", NivelEstudio.SECUNDARIO));
-        e.agregarPersona(new Persona("Sara", "Jerez", NivelEstudio.SECUNDARIO));
-
-        e.mostrarPorNivel(NivelEstudio.UNIVERSITARIO);
+        // metodos que pueden habarcar los enum
+        NivelEstudio[] niveles = NivelEstudio.values();
+        for (int i = 0; i < niveles.length; i++) {
+            System.out.print(niveles[i]+" | ");
+        }
+        //
+        System.out.println();
+        int ver = NivelEstudio.POSGRADO.compareTo(NivelEstudio.POSGRADO);
+        System.out.println(ver);
+        System.out.println();
+        System.out.println(NivelEstudio.PRIMARIO.name());
     }
 }
