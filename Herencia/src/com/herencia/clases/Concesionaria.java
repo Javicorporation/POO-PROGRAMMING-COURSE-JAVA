d64bcf51c2132879274a829f942b43ce63c17992
Javicorporation <krchipullajavier@gmail.com>
2024-08-23 14:11:00 -0500

    Upcasting - sin herencia no hay upcasting -  la ventaja es que generalizá los objetos y los podemos guardar en una sola lista.

---
+++ Herencia/src/com/herencia/clases/Concesionaria.java
@@ -0,0 +1,34 @@
+package com.herencia.clases;
+
+import com.herencia.vehiculo.Vehiculo;
+
+import java.util.ArrayList;
+
+public class Concesionaria {
+
+    // Manera normal de instanciar objetos, sin uso de upcasting o generalización
+    /*private ArrayList<Auto> autos;
+    private ArrayList<Moto> motod;
+
+    public Conscesionario() {
+        this.autos = new ArrayList<>();
+        this.motod = new ArrayList<>();
+    }
+
+    public void agregarAuto(Auto auto) {
+        this.autos.add(auto);
+    }
+    public void agregarMoto(Moto moto) {
+        this.motod.add(moto);
+    }*/
+
+    // forma de upcasting o generalizacion
+    ArrayList<Vehiculo> v ;
+    public Concesionaria() {
+        this.v = new ArrayList<>();
+    }
+
+    public void agregarVehiculo(Vehiculo v) {
+        this.v.add(v);
+    }
+}
--- Herencia/src/com/herencia/test/Test.java
+++ Herencia/src/com/herencia/test/Test.java
@@ -1,19 +1,25 @@
 package com.herencia.test;
 
+import com.herencia.clases.Concesionaria;
 import com.herencia.vehiculo.Auto;
 import com.herencia.vehiculo.Moto;
+import com.herencia.vehiculo.Vehiculo;
 
 public class Test {
 
     public static void main(String[] args) {
-        Auto auto = new Auto("Toyota","6534732", "CBNVHD", true);
-        auto.frenar();
-        System.out.println(auto);
+//        Vehiculo vehiculo = new Vehiculo("x","conpacto", "TYDVEHEH");
+//        Auto auto = new Auto("Toyota","6534732", "CBNVHD", true);
+////        auto.frenar();
+////        System.out.println(auto);
+//        Moto moto = new Moto("Toyota","6534732", "CBNVHD", 23);
+////        moto.acelerar();
+////        moto.getMarca();
+////        System.out.println(moto);
+//        Vehiculo auto2 = new Auto("Toyota","6534732", "CBNVHD", true);
 
-        Moto moto = new Moto("Toyota","6534732", "CBNVHD", 23);
-        moto.acelerar();
-        moto.getMarca();
-        System.out.println(moto);
-
+        Concesionaria c = new Concesionaria();
+        c.agregarVehiculo(new Auto("Toyota","6534732", "CBNVHD", true));
+        c.agregarVehiculo(new Moto("Toyota","6534732", "CBNVHD", 23));
     }
 }
