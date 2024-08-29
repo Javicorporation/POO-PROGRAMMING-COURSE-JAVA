6692907121257dc8bfb6209c1f2dd49f69ae032e
Javicorporation <krchipullajavier@gmail.com>
2024-08-29 13:34:07 -0500

    Uso de final - los atributos final “contaste” no pueden quedar sin inicializar, no generan get y set. - las clases abstractas no pueden ser final - los métodos abstractos no pueden ser final

---
+++ Metodo Final/src/com/finaaaaal/Test.java
@@ -0,0 +1,11 @@
+package com.finaaaaal;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        //los atributos final debe ser inicializada.
+        final int num1 = 0;
+        final int MUNEROX = 1;
+    }
+}
