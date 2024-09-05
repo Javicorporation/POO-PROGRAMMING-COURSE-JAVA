7e1adf634c44c65afee861d27c4e8337af99c86e
Javicorporation <krchipullajavier@gmail.com>
2024-09-05 09:44:47 -0500

    implementacion de tryCatsh - uso de finally para la liberacion de recursos. - siempre que sea necesario usar tryCatch - se puede mejorar

--- excepciones checked y unchecked/src/com/exce/Test.java
+++ excepciones checked y unchecked/src/com/exce/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
         VisorDeTxt v = new VisorDeTxt();
         try {
-            v.mostrarTxt("C:\\Users\\krchi\\OneDrive - Universidad de Guayaquil\\CURSOS - PROTECTOS\\JAVA\\JAVAPOO\\excepciones checked y unchecked\\src\\com\\exce\\hola.txt");
+            v.mostrarTxt("C:\\Users\\krchi\\OneDrive - Universidad de Guayaquil\\CURSOS - PROTECTOS\\JAVA\\JAVAPOO\\excepciones checked y unchecked\\src\\com\\exce\\hola.tx");
         }catch (FileNotFoundException ex){
             System.out.println("El archivo no existe");
         }catch (IOException e){
--- excepciones checked y unchecked/src/com/exce/VisorDeTxt.java
+++ excepciones checked y unchecked/src/com/exce/VisorDeTxt.java
@@ -6,9 +6,25 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class VisorDeTxt {
+    // implementacion de excepciones
     public void mostrarTxt(String ruta) throws IOException {
         File elArchivo = new File(ruta);
-        BufferedReader br = new BufferedReader(new FileReader(elArchivo));
-        System.out.println(br.readLine());
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(elArchivo));
+            System.out.println(br.readLine());
+            // IOException de abarca las entradas y salidas
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            throw new RuntimeException("error inesperado en el catch");
+        }finally {  // uso del final para liberar recursos(el recurso que queramos liberar debe estar fuera del tryCatch, en este caso el BufferedReader)
+            try {
+                br.close();
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 }
