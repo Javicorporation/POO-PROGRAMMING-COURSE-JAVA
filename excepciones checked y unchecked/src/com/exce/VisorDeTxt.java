7eb46fec9565d180c274238c37f4c673b8760fe7
Javicorporation <krchipullajavier@gmail.com>
2024-09-05 09:52:40 -0500

    TRY-WITH-RESOURCES - este método tiene una sintaxis algo diferente el cual se puede autocerrar los procesos o liberar los recursos

--- excepciones checked y unchecked/src/com/exce/VisorDeTxt.java
+++ excepciones checked y unchecked/src/com/exce/VisorDeTxt.java
@@ -6,25 +6,25 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class VisorDeTxt {
-    // implementacion de excepciones
+    // implementacion de TRY-WITH-RESOURCES
     public void mostrarTxt(String ruta) throws IOException {
         File elArchivo = new File(ruta);
-        BufferedReader br = null;
+        //BufferedReader br = null;
 
-        try {
-            br = new BufferedReader(new FileReader(elArchivo));
+        try (BufferedReader br = new BufferedReader(new FileReader(elArchivo));){
             System.out.println(br.readLine());
             // IOException de abarca las entradas y salidas
         }catch (IOException e){
             System.out.println(e.getMessage());
             throw new RuntimeException("error inesperado en el catch");
-        }finally {  // uso del final para liberar recursos(el recurso que queramos liberar debe estar fuera del tryCatch, en este caso el BufferedReader)
-            try {
-                br.close();
-            }catch (IOException e){
-                System.out.println(e.getMessage());
-            }
         }
+//        }finally {  // uso del final para liberar recursos(el recurso que queramos liberar debe estar fuera del tryCatch, en este caso el BufferedReader)
+//            try {
+//                br.close();
+//            }catch (IOException e){
+//                System.out.println(e.getMessage());
+//            }
+//        }
 
     }
 }
