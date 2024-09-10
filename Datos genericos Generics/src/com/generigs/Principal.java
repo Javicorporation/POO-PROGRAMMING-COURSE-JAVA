08cbe06390c10482714626954ede08b4b9193db3
javicorporation <krchipullajavier@gmail.com>
2024-09-10 14:50:12 -0500

    GENERICS - los nombres de los tipos genericos siempre se escriben con mayuscula. - los genericos siempre tendran "<>" - no soportan datos primitivos

---
+++ Datos genericos Generics/src/com/generigs/Principal.java
@@ -0,0 +1,10 @@
+package com.generigs;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList<Auto> autos = new ArrayList<>();
+        ArrayList<Persona> personas = new ArrayList<>();
+    }
+}
