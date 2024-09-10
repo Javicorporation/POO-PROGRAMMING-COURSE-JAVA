797cfa83759e365855aa1828ebec44a3c43e4a69
javicorporation <krchipullajavier@gmail.com>
2024-09-10 15:48:46 -0500

    Creando un generic -Creación de un atributo random para realizar el método get - creación del método remove para que al momento de seleccionar un elemento se remueva de la lista

--- crear coleccion usando generics/src/com/genericos/Sorteador.java
+++ crear coleccion usando generics/src/com/genericos/Sorteador.java
@@ -1,20 +1,43 @@
 package com.genericos;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Sorteador<T> {
+
+    // atributos
+    // podemos usar encapsulamiento
+    ArrayList<T> elementos;
+    Random randon;
+
+    // implementar un constructor
+
+
+    public Sorteador() {
+        this.elementos = new ArrayList<>();
+        this.randon = new Random();
+    }
 
     // agregar elemneto
     public void add(T elemento){
-
+        this.elementos.add(elemento);  // delegamos acción
     }
 
     // obtener un elemento
     public T get(){
-        return null;
+        int rnd = randon.nextInt(elementos.size());
+        return elementos.get(rnd);
+    }
+
+    // remover un elemento
+    public T remove(){
+        int rnd = randon.nextInt(elementos.size());
+        return elementos.remove(rnd);
     }
 
 
     // retornar si esta o no
     public boolean isEmpty(){
-        return false;
+        return elementos.isEmpty(); // delegamos acción
     }
 }
--- crear coleccion usando generics/src/com/genericos/Test.java
+++ crear coleccion usando generics/src/com/genericos/Test.java
@@ -4,6 +4,14 @@ public class Test {
     public static void main(String[] args) {
         Sorteador<String> sorteador = new Sorteador<>();
 
+        sorteador.add("hola");
+        sorteador.add("que tal");
+        sorteador.add("pau");
+
+        while(!sorteador.isEmpty()) {
+            System.out.println("El proximo es: "+sorteador.remove());
+        }
+
     }
     public static void retardo(int s) {
         try {
