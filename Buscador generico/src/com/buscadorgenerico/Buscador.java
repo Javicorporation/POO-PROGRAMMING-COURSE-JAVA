eb65ff621d2054701acafa5a0c23a6309826a22f
javicorporation <krchipullajavier@gmail.com>
2024-09-13 16:50:04 -0500

    implemantación del buscado en el test en la clase persona y autos

--- Buscador generico/src/com/buscadorgenerico/Buscador.java
+++ Buscador generico/src/com/buscadorgenerico/Buscador.java
@@ -6,7 +6,9 @@ import java.util.Iterator;
 public class Buscador <T extends Identificable, K>{
     public T buscar(Collection<? extends T>elemento, K id){
 
+        // ponemos un atributo generico en null
         T encontrado = null;
+        //implementamos el iterador
         Iterator<? extends T> iterator = elemento.iterator();
         while(iterator.hasNext() && encontrado == null){
             T actual = iterator.next();
--- Buscador generico/src/com/buscadorgenerico/Test.java
+++ Buscador generico/src/com/buscadorgenerico/Test.java
@@ -1,4 +1,35 @@
 package com.buscadorgenerico;
 
+import java.util.ArrayList;
+
 public class Test {
+    public static void main(String[] args) {
+        ArrayList <Persona> personas = new ArrayList<>();
+        ArrayList <Auto> autos = new ArrayList<>();
+
+
+        autos.add(new Auto("BCD234","VMW", "Verde"));
+        autos.add(new Auto("DEF458", "Toyota", "Blanco"));
+        autos.add(new Auto("CDE345","MAS", "Azul"));
+        autos.add(new Auto("DEF457","VMW", "Gris"));
+
+        Buscador<Auto, String> BuscadorDeAuto;
+        BuscadorDeAuto = new Buscador<>();
+        Auto a = BuscadorDeAuto.buscar(autos,"DEF457");
+        System.out.println(a);
+
+
+        personas.add(new Persona(1234, "Pepe", "Gomez", 18));
+        personas.add(new Persona(1596, "Ana", "Perez",24));
+        personas.add(new Persona(7645, "Luis", "Lopez", 21));
+        personas.add(new Persona(2345, "Lía", "Alvarez", 19));
+        personas.add(new Persona(4763, "Sara", "Britez",20));
+
+        Buscador<Persona, Integer> buscadorDePersona;
+        buscadorDePersona = new Buscador<>();
+        Persona persona = buscadorDePersona.buscar(personas,2345);
+        System.out.println(persona);
+
+
+    }
 }
