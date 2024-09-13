0e01470ba34436db52d9356514246b07fae9437a
javicorporation <krchipullajavier@gmail.com>
2024-09-13 16:30:34 -0500

    implementacion de la intefaz identificable en la clase Auto y Persona

--- Buscador generico/src/com/buscadorgenerico/Auto.java
+++ Buscador generico/src/com/buscadorgenerico/Auto.java
@@ -1,4 +1,62 @@
 package com.buscadorgenerico;
 
-public class Auto {
+public class Auto implements Identificable<String>{
+    private String patente;
+    private String marca;
+    private String color;
+
+    public Auto(String patente, String marca, String color) {
+        this.patente = patente;
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "patente='" + patente + '\'' +
+                ", marca='" + marca + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public String getId() {
+        return patente;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.patente = id;
+    }
+
+    @Override
+    public boolean sameId(String id) {
+        return this.patente.equals(id);
+    }
 }
--- Buscador generico/src/com/buscadorgenerico/Persona.java
+++ Buscador generico/src/com/buscadorgenerico/Persona.java
@@ -1,4 +1,59 @@
 package com.buscadorgenerico;
 
-public class Persona {
+public class Persona implements Identificable<Integer> {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona(int  id, String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean sameId(Integer id) {
+        return this.id == id;
+    }
 }
