956144fcdbb229c43c1c3dd4b3e4cabdcb31549e
javicorporation <krchipullajavier@gmail.com>
2024-09-16 17:42:24 -0500

    proyecto de 3 java apiRest - creamos el archivo .env (variables de entorno), en este archivo van los datos delicados de la aplicación. - creamos una carpeta para la base de datos - los implementamos en gitignore

---
+++ apiResr/src/main/java/com/aplicacion/apiresr/ApiResrApplication.java
@@ -0,0 +1,11 @@
+package com.aplicacion.apiresr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiResrApplication.class, args);
+    }
+}
