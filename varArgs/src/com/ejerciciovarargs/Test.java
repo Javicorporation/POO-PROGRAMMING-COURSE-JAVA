57ffb6134d929e65affc201525c73b7d646f98d8
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-09-18 17:58:35 -0500

    StringBuilder -uso de sus metodos

--- varArgs/src/com/ejerciciovarargs/Test.java
+++ varArgs/src/com/ejerciciovarargs/Test.java
@@ -3,7 +3,8 @@ package com.ejerciciovarargs;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println(sumar("hola",4,8,9,6));
+        //System.out.println(sumar("hola",4,8,9,6));
+        ejemplo();
     }
                                             // el parametro indefinido siempre va al final de todo
     public static int sumar(String nombre, int... numeros){
@@ -13,5 +14,17 @@ public class Test {
             suma += num;
         }
         return suma;
+    }
+
+    public static void ejemplo(){
+        StringBuilder vacio = new StringBuilder();
+        System.out.println(vacio);
+
+        vacio.append("hola ");
+        vacio.append("que tal");
+        vacio.deleteCharAt(vacio.length()-1);
+        vacio.reverse();
+        vacio.appendCodePoint(1);
+        System.out.println(vacio);
     }
 }
