e34f4c2ccca08133d93d9963f63f7ec03bd97877
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-09-20 13:51:49 -0500

    Múltiples Excepciones - uso múltiplo de catch para diferentes excepciones, segun la excepción correspondiente, siempre se ejecutará el finally

--- excepciones/src/com/excepcionessss/Test.java
+++ excepciones/src/com/excepcionessss/Test.java
@@ -1,26 +1,38 @@
 package com.excepcionessss;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         Scanner getting = new Scanner(System.in);
+        boolean loop = false;
 
 
+        do {
+            try {
+                System.out.print("Ingrese el numero 1: ");
+                int n1 = getting.nextInt();
+                System.out.print("Ingrese el numero 2: ");
+                int n2 = getting.nextInt();
+                getting.nextLine();
 
-        try {
-            System.out.print("Ingrese el numero 1: ");
-            int n1 = getting.nextInt();
-            System.out.print("Ingrese el numero 2: ");
-            int n2 = getting.nextInt();
-            getting.nextLine();
+                getting.close();
+                int resultado = n1/n2;
+                System.out.println(resultado);
+                loop = true;
+            }catch (InputMismatchException e) {
+                System.out.println("Ocurrio un error, ingrese solo numeros enteros.");
+                e.printStackTrace();
+                getting.nextLine();
+            }catch (ArithmeticException e){
+                System.out.println("Ocurrio un error, ino se puede dividir entre 0.");
+            }finally {
+                getting.nextLine();
+                getting.close();
 
-            getting.close();
-            int resultado = n1/n2;
-            System.out.println(resultado);
-        }catch (Exception e) {
-            System.out.println("Ocurrio un error, ingrese solo numeros enteros.");
-            e.printStackTrace();
-        }
+            }
+        }while (!loop);
+
     }
 }
