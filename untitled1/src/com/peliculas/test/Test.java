ee556100e069dc952c9f1baf7892c1ece5d31752
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-09-23 10:42:42 -0500

    creación de ejecutable java - implementación de java swing - entrada y salida

--- untitled1/src/com/peliculas/test/Test.java
+++ untitled1/src/com/peliculas/test/Test.java
@@ -1,22 +1,12 @@
 package com.peliculas.test;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
 
-        ArrayList<String> meses = new ArrayList<>();
-        meses.add("M");
-        meses.add("F");
-        meses.add("H");
-
-        for (String dato : meses) {
-            System.out.println(dato);
-        }
-
-        // esta sseria otra forma de usar el foreach
-        meses.forEach(System.out::println);
-
-        //System.out.println(meses);
+        String valor = JOptionPane.showInputDialog(null, "Ingrese un texto: ","Entrada",3);
+        JOptionPane.showMessageDialog(null, valor,"error..",JOptionPane.ERROR_MESSAGE);
     }
 }
