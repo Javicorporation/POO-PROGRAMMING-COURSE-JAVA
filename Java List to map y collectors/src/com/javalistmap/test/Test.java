c5974795efabfad48d1ee347d9e61215024e1170
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-09-25 15:06:47 -0500

    Java List to Map - convertir una lista a un map - si la lista es corta la forma anterior funcionaria muy bien, pero como nunca va a ser corta daria problemas. - por lo que tendriamos que convertir la lista a un map de forma directa por medio de stream. - Los mapas nos permiten acceder mucho más rápido a un elemento a través de su hash ya que accedemos a una sublista. - De esta forma será mucho más rápido y mucho más sencillo acceder a los datos de un elemento concreto a través de del uso de su identificador.

--- Java List to map y collectors/src/com/javalistmap/test/Test.java
+++ Java List to map y collectors/src/com/javalistmap/test/Test.java
@@ -2,10 +2,8 @@ package com.javalistmap.test;
 
 import com.javalistmap.model.Coche;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
@@ -14,15 +12,25 @@ public class Test {
         Coche modelo2 = new Coche("Y2Yaris", "Toyota", 20000);
         Coche modelo3 = new Coche("Y3Yaris", "Toyota", 22000);
 
+        //Lista
         List<Coche> coches = Arrays.asList(modelo1, modelo2, modelo3);
 
         // para muscar un objeto por medio de su id
-        Optional<Coche> optional = coches.stream()
-                .filter(m -> m.getIdentificador().equals("Y1Yaris")).findFirst();
-        if (optional.isPresent()) {
-            System.out.println(optional.get().getIdentificador());
-            System.out.println(optional.get().getMarca());
-            System.out.println(optional.get().getPrecio());
-        }
+//        Optional<Coche> optional = coches.stream()
+//                .filter(m -> m.getIdentificador().equals("Y1Yaris")).findFirst();
+//        if (optional.isPresent()) {
+//            System.out.println(optional.get().getIdentificador());
+//            System.out.println(optional.get().getMarca());
+//            System.out.println(optional.get().getPrecio());
+//        }
+
+        // convertimos la lista a un map
+        Map<String, Coche> mapa = coches.stream()
+                .collect(Collectors.toMap(x -> x.getIdentificador(), x -> x));
+
+        Coche nuevoCoche = mapa.get("Y1Yaris");
+
+        System.out.println(nuevoCoche.getMarca());
+        System.out.println(nuevoCoche.getPrecio());
     }
 }
