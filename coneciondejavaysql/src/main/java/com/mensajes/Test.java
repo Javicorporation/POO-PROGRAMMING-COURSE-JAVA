a040f65533f8cd04a364346d624d7869cd5fb19a
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-01 15:58:34 -0500

    JAVA - MYSQL - Implementación de exception throw SQLException - Creación de variable para la conexión - mostrar un mensaje de conectado

--- coneciondejavaysql/src/main/java/com/mensajes/Test.java
+++ coneciondejavaysql/src/main/java/com/mensajes/Test.java
@@ -1,4 +1,14 @@
 package com.mensajes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Test {
+    // implementacion de excepcion
+    public static void main(String[] args) throws SQLException {
+        // creamos una variavle para realizar conexiòn de base de datos
+        Connection conectarsql = DriverManager.getConnection("jdbc:mysql://localhost/tiendadb?serverTimezone=UTC","root", "12345");
+        System.out.println("conectado");
+    }
 }
