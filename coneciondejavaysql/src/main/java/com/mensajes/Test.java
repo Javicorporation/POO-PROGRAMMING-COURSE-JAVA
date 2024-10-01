65da8da6533494abccf05bb3218563d798a56ad6
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-01 17:31:22 -0500

    JAVA - MYSQL - Obtención de registros de la tabla. - Creación de la variable sql con la petición. - Presentación de los datos por medio de un while. - cerrar siempre los servicios

--- coneciondejavaysql/src/main/java/com/mensajes/Test.java
+++ coneciondejavaysql/src/main/java/com/mensajes/Test.java
@@ -1,8 +1,6 @@
 package com.mensajes;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Test {
     // implementacion de excepcion
@@ -10,5 +8,32 @@ public class Test {
         // creamos una variavle para realizar conexiòn de base de datos
         Connection conectarsql = DriverManager.getConnection("jdbc:mysql://localhost/tiendadb?serverTimezone=UTC","root", "12345");
         System.out.println("conectado");
+
+
+        // variable de sentencia
+        String sql = "SELECT * FROM mensajes";
+        // variable de implementación de la conexión
+        PreparedStatement pst = conectarsql.prepareStatement(sql);
+        // resultado de la query
+        ResultSet rs = pst.executeQuery();
+
+
+        // recorremos  la query
+        while(rs.next()){
+            //implenetaacion de los datos
+            String nombre = rs.getString("nombre");
+            String direccion = rs.getString("direccion");
+            String ciudad = rs.getString("ciudad");
+            System.out.printf("%s %s %s \n", nombre, direccion, ciudad);
+        }
+
+        // cerrar servicios
+        rs.close();
+        pst.close();
+        conectarsql.close();
+
+
     }
+
+
 }
