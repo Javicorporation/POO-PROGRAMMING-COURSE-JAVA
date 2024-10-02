ed12c18ecc9439212499b2595862c84b3fbcf08a
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-02 10:13:33 -0500

    JAVA - MYSQL - actualización de un registo por medio de java - uso de .executeUptade

--- coneciondejavaysql/src/main/java/com/mensajes/Test.java
+++ coneciondejavaysql/src/main/java/com/mensajes/Test.java
@@ -7,12 +7,14 @@ public class Test {
     public static void main(String[] args) throws SQLException {
 
         listarRegistros();
-        ingresarRegistros("Maria","La 8", "Guayaquil");
+        System.out.println();
+        editarRegistro("Maria", "Bastion", "Guayaquil");
         System.out.println();
         listarRegistros();
     }
 
 
+    // insertar un registro desde java
     public static void ingresarRegistros(String nombre, String direccion, String ciudad) throws SQLException {
         Connection conectado = DriverManager.getConnection("jdbc:mysql://localhost/tiendadb?serverTimezone=UTC", "root", "12345");
 
@@ -30,11 +32,7 @@ public class Test {
     }
 
 
-
-
-
-
-
+    // mostrar los registros desde java
     public static void listarRegistros() throws SQLException {
         // creamos una variavle para realizar conexiòn de base de datos
         Connection conectarsql = DriverManager.getConnection("jdbc:mysql://localhost/tiendadb?serverTimezone=UTC","root", "12345");
@@ -64,5 +62,23 @@ public class Test {
         conectarsql.close();
     }
 
+
+
+
+    public static void editarRegistro(String direccion, String ciudad, String nombre) throws SQLException {
+        Connection conectar = DriverManager.getConnection("jdbc:mysql://localhost/tiendadb?serverTimezone=UTC", "root", "12345");
+        System.out.println("Conexion exitosa");
+        String sql = "UPDATE mensajes SET direccion = ?, ciudad = ? WHERE nombre = ?";
+        PreparedStatement pst = conectar.prepareStatement(sql);
+
+        pst.setString(1, direccion);
+        pst.setString(2, ciudad);
+        pst.setString(3, nombre);
+        // sirve para actualizar la tabla
+        pst.executeUpdate();
+
+        pst.close();
+        conectar.close();
+    }
 
 }
