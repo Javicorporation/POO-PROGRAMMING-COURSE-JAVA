1356c6a580aa6dd4943e19504de93c7356d890f6
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-07 17:00:00 -0500

    uso de encapsulamiento - uso de modificadores de acceso. - uso de get y set

---
+++ uso de encapsulamiento/src/com/pruebas/Alumno.java
@@ -0,0 +1,45 @@
+package com.pruebas;
+
+public class Alumno {
+
+    // no se ve pero estan publicos en su paquete
+    // los atributos siempre tienen que ser privados
+    int id;
+    String nombre;
+    String apellido;
+
+
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
---
+++ uso de encapsulamiento/src/com/pruebas/Encapsulamiento.java
@@ -0,0 +1,8 @@
+package com.pruebas;
+
+public class Encapsulamiento {
+    public static void main(String[] args) {
+        Alumno alu1 = new Alumno();
+        Alumno alu2 = new Alumno(1, "Juanin","Martir");
+    }
+}
