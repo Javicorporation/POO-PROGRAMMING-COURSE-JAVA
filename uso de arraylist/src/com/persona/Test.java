ec09dff77a8cc58ca7376ce7138ce8e14421f892
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-09 18:09:16 -0500

    LinkedList y ArrayList - la eliminacion es diferente en cada uno - la agragacion es similar

--- uso de arraylist/src/com/persona/Test.java
+++ uso de arraylist/src/com/persona/Test.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+
         // inizializacion de arrayList
         List<Persona> personas = new LinkedList<>();
         personas.add(new Persona(1, "juanin", "carla", 21));
@@ -14,7 +15,30 @@ public class Test {
         personas.add(new Persona(4, "Maria", "carla", 21));
         // para agregar al principio se le agrega un parametro entero al inicio
         personas.add(0,new Persona(8, "Lisa", "carla", 21));
+        //personas.forEach(v -> System.out.println(v.toString()));
+        // remover en linkedList
+        String nombreABorrar = "paula";
+        for (Persona persona : personas) {
+            if(persona.getNombre().equals(nombreABorrar)){
+                personas.remove(persona);
+                break;
+            }
+        }
+        for (Persona persona : personas) {
+            System.out.println("prueba "+persona.getNombre());
+        }
 
-        personas.forEach(v -> System.out.println(v.toString()));
+        System.out.println();
+        // inizializacion de LinkedList
+        List<Persona> usuarios = new ArrayList<>();
+        usuarios.add(new Persona(1, "carlin", "carla", 21));
+        usuarios.add(new Persona(2, "mario", "carla", 21));
+        usuarios.add(new Persona(3, "pablito", "carla", 21));
+        usuarios.add(new Persona(4, "cali", "carla", 21));
+
+        //remover de l lista
+        usuarios.remove(1);
+        // recorrer lista
+        usuarios.forEach(v -> System.out.println(v.toString()));
     }
 }
