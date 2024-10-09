a52eb83ff9eb9a8c4205ad8332c47b735ff10cb5
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-09 17:20:54 -0500

    LinkedList - no tiene indices fijos - se pueden agregar elementos al principio y al final

--- uso de arraylist/src/com/persona/Test.java
+++ uso de arraylist/src/com/persona/Test.java
@@ -1,15 +1,19 @@
 package com.persona;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         // inizializacion de arrayList
-        List<Persona> personas = new ArrayList<>();
+        List<Persona> personas = new LinkedList<>();
         personas.add(new Persona(1, "juanin", "carla", 21));
         personas.add(new Persona(2, "paula", "carla", 21));
         personas.add(new Persona(3, "karen", "carla", 21));
+        personas.add(new Persona(4, "Maria", "carla", 21));
+        // para agregar al principio se le agrega un parametro entero al inicio
+        personas.add(0,new Persona(8, "Lisa", "carla", 21));
 
         personas.forEach(v -> System.out.println(v.toString()));
     }
