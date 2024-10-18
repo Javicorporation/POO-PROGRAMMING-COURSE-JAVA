155dc53be88d19abbf038bd83a78ff83fcd16ee1
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-17 22:19:07 -0500

    creacion de la tabla de la base de datos por medio de el controller

---
+++ proyectojpa/src/main/java/com/mycompany/proyectojpa/Proyectojpa.java
@@ -0,0 +1,11 @@
+package com.mycompany.proyectojpa;
+
+import com.mycompany.proyectojpa.persistencia.ControladoraPersistencia;
+
+
+public class Proyectojpa {
+
+    public static void main(String[] args) {
+        ControladoraPersistencia controper = new ControladoraPersistencia();
+    }
+}
