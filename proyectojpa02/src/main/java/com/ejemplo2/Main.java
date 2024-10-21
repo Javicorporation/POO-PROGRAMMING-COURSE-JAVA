2d25df1148c8227d5f095d31e0882a966a64ebae
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-21 18:30:01 -0500

    Hibernate - JPA - JAVA otra forma de configuracion de XML - creamos una clase configuracion y le implementamos la interface PersistenceUnitInfo - implementamos la dependencia de HikariCP

--- proyectojpa02/src/main/java/com/ejemplo2/Main.java
+++ proyectojpa02/src/main/java/com/ejemplo2/Main.java
@@ -1,27 +1,39 @@
 package com.ejemplo2;
 
+import com.ejemplo2.config.MyPersistenUnitInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+        Map<String, String> properties = new HashMap<>();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        try {
+            EntityManagerFactory emf
+                    = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new MyPersistenUnitInfo(), properties);
 
-        EntityManager em = null;
-        em = entityManagerFactory.createEntityManager();
+        EntityManager em  = emf.createEntityManager();
 
         em.getTransaction().begin();
 
         Student student = new Student();
 
-        student.setId(4);
-        student.setName("Juanin");
+        student.setId(8);
+        student.setName("Pablito");
 
         em.persist(student);
 
         em.getTransaction().commit();
+        }catch (Exception e) {
+        e.printStackTrace();
+    }
 
     }
 }
---
+++ proyectojpa02/src/main/java/com/ejemplo2/config/MyPersistenUnitInfo.java
@@ -0,0 +1,104 @@
+package com.ejemplo2.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+public class MyPersistenUnitInfo implements PersistenceUnitInfo {
+    @Override
+    public String getPersistenceUnitName() {
+        return "org.hibernate.jpa.HibernatePersistenceProvider";
+    }
+
+    @Override
+    public String getPersistenceProviderClassName() {
+        return "";
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+        return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:postgresql://localhost:3306/basejpa");
+        ds.setUsername("postgres");
+        ds.setPassword("12345");
+        return ds;
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMappingFileNames() {
+        return List.of("com.ejemplo2");
+    }
+
+    @Override
+    public List<URL> getJarFileUrls() {
+        return List.of();
+    }
+
+    @Override
+    public URL getPersistenceUnitRootUrl() {
+        return null;
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+        return List.of();
+    }
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return false;
+    }
+
+    @Override
+    public SharedCacheMode getSharedCacheMode() {
+        return null;
+    }
+
+    @Override
+    public ValidationMode getValidationMode() {
+        return null;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return null;
+    }
+
+    @Override
+    public String getPersistenceXMLSchemaVersion() {
+        return "";
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer classTransformer) {
+
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}
