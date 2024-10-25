2f254b293719dbf45ca847a0f632d5b788ad727c
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-25 00:07:12 -0500

    - Creación de la base de datos - como crear una nueva conexión de por medio de netbeans - como crear una unidad de persistencia

---
+++ mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject0;
+
+/**
+ *
+ * @author krchi
+ */
+public class Mavenproject0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
