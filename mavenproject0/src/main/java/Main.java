c5663d28c37df53cf04705375135b84ade5425e7
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-25 00:56:03 -0500

    - creacion de tablas por medio de la entidad

---
+++ mavenproject0/src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+import com.mycompany.mavenproject0.persistencia.ControladoraPersistencia;
+
+public class Main {
+    public static void main(String[] args) {
+        ControladoraPersistencia  contrala = new ControladoraPersistencia();
+    }
+}
