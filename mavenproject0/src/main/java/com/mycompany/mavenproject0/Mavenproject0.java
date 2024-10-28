3f4ef9ea30355d46c617a8f9d28e02df5476e9ef
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-28 17:52:12 -0500

    - creacion de tablas por medio de la entidad (corregido ya genera la tabla)

--- mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
+++ mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.mavenproject0;
 
+import com.mycompany.mavenproject0.persistencia.ControladoraPersistencia;
+
 /**
  *
  * @author krchi
@@ -11,6 +13,6 @@ package com.mycompany.mavenproject0;
 public class Mavenproject0 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ControladoraPersistencia  contrala = new ControladoraPersistencia();
     }
 }
