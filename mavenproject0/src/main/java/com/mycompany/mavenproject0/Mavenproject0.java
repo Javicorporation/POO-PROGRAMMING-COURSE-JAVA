356467ff8ffb408e6e1b03e6f5f234109a0df2ba
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-29 18:46:03 -0500

    - implementacion de metodos en el controlador de carrera - asignacion de carrera a alumno

--- mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
+++ mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
@@ -1,8 +1,10 @@
 package com.mycompany.mavenproject0;
 import com.mycompany.mavenproject0.logica.Alumno;
+import com.mycompany.mavenproject0.logica.Carrera;
 import com.mycompany.mavenproject0.logica.Controladora;
 import com.mycompany.mavenproject0.persistencia.ControladoraPersistencia;
 import java.util.Date;
+import java.util.List;
 
 
 public class Mavenproject0 {
@@ -10,12 +12,26 @@ public class Mavenproject0 {
         
         Controladora controladora = new Controladora();
         
-        Alumno alumno1 = new Alumno(10, "Carlitos", "Paz", new Date());
+        Carrera carrera1 = new Carrera(1, "Administracion");
+        controladora.crearCarrera(carrera1);
+        
+        Alumno alumno1 = new Alumno(5, "Martha", "Luz", new Date(), carrera1);
         controladora.crearAlumno(alumno1);
         
         //controladora.eliminarAlumno(1);
         
-        alumno1.setApellido("Marss");
-        controladora.editarAlumno(alumno1);
+//        alumno1.setApellido("Marss");
+//        controladora.editarAlumno(alumno1);
+
+//          Alumno alumno1 = controladora.traerAlumno(2);
+//          System.out.println(alumno1);
+          
+          System.out.println("-----------------");
+          
+          List<Alumno> alumnos = controladora.traerListaAlumnos();
+          
+          for(Alumno alu : alumnos){
+              System.out.println("- "+alu);
+          }
     }
 }
