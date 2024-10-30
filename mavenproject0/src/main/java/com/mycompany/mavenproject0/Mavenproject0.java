5a6d6f0b9979030876440ace89561e0d3549fb81
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-10-30 11:01:24 -0500

    - explicación - si ya tenemos creadas las bases de datos y recién implementamos las relaciones, tenemos que borrar las tablas - primero se crean las entidades que no tienen relaciones - a nivel de base se datos la relacion es unidireccional y se crear una base de datos adicional

--- mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
+++ mavenproject0/src/main/java/com/mycompany/mavenproject0/Mavenproject0.java
@@ -2,8 +2,10 @@ package com.mycompany.mavenproject0;
 import com.mycompany.mavenproject0.logica.Alumno;
 import com.mycompany.mavenproject0.logica.Carrera;
 import com.mycompany.mavenproject0.logica.Controladora;
+import com.mycompany.mavenproject0.logica.Materia;
 import com.mycompany.mavenproject0.persistencia.ControladoraPersistencia;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -12,7 +14,24 @@ public class Mavenproject0 {
         
         Controladora controladora = new Controladora();
         
-        Carrera carrera1 = new Carrera(1, "Administracion");
+        // instanciamos materias
+        Materia materia1 = new Materia(1, "Matematicas", "Escrito");        
+        Materia materia2 = new Materia(2, "Lenguaje", "Virtual");
+        Materia materia3 = new Materia(3, "Historia", "Escrito");
+
+        // creamos las materias
+        controladora.crearMateria(materia1);
+        controladora.crearMateria(materia2);
+        controladora.crearMateria(materia3);
+        
+        // agregamos a la lista
+        List<Materia> listaMaterias = new LinkedList<>();
+        listaMaterias.add(materia1);        
+        listaMaterias.add(materia2);
+        listaMaterias.add(materia3);
+
+        
+        Carrera carrera1 = new Carrera(1, "Administracion", listaMaterias);
         controladora.crearCarrera(carrera1);
         
         Alumno alumno1 = new Alumno(5, "Martha", "Luz", new Date(), carrera1);
@@ -28,10 +47,10 @@ public class Mavenproject0 {
           
           System.out.println("-----------------");
           
-          List<Alumno> alumnos = controladora.traerListaAlumnos();
-          
-          for(Alumno alu : alumnos){
-              System.out.println("- "+alu);
-          }
+//          List<Alumno> alumnos = controladora.traerListaAlumnos();
+//          
+//          for(Alumno alu : alumnos){
+//              System.out.println("- "+alu);
+//          }
     }
 }
