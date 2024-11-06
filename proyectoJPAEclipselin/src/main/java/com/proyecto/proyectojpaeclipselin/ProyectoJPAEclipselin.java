e595de52beeea099cdfbcf648e82e4c114d41d55
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-06 16:51:44 -0500

    crud con jpa y eclipselin desde NetBeans - creamos un archivo META-XML - creamos la conexion y conectamos netbeans con la base de datos como url, user, password - creamos los paquetes del protecto.

---
+++ proyectoJPAEclipselin/src/main/java/com/proyecto/proyectojpaeclipselin/ProyectoJPAEclipselin.java
@@ -0,0 +1,8 @@
+package com.proyecto.proyectojpaeclipselin;
+
+public class ProyectoJPAEclipselin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
