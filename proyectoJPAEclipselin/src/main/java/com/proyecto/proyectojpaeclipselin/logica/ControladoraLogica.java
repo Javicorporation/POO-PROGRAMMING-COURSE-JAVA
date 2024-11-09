5698c1274e9fb72e5100953ace01e39b75ffc6b7
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-08 21:05:57 -0500

    crud con jpa y eclipselin desde NetBeans - creacion del metodo traer un alumno y traer una lista de alumnos - casteo de una lista a un arraylist

---
+++ proyectoJPAEclipselin/src/main/java/com/proyecto/proyectojpaeclipselin/logica/ControladoraLogica.java
@@ -0,0 +1,38 @@
+package com.proyecto.proyectojpaeclipselin.logica;
+
+// esta clase va a implementar los metodos
+
+import com.proyecto.proyectojpaeclipselin.persitencie.ControladoraDePersistencia;
+import com.proyecto.proyectojpaeclipselin.persitencie.exceptions.NonexistentEntityException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControladoraLogica {
+    
+    ControladoraDePersistencia controlDePersistencia = new ControladoraDePersistencia();
+    
+    
+    public void crearAlumno(Alumno alumno) {
+        controlDePersistencia.crearAlumno(alumno);
+    }
+    
+    public void eliminar(int id) throws NonexistentEntityException{
+        controlDePersistencia.eliminar(id);
+    }
+    
+    public void editar(Alumno alumno) throws NonexistentEntityException{
+        controlDePersistencia.editar(alumno);
+    }
+    
+    public Alumno traerAlumno(int id){
+        return controlDePersistencia.traerAlumno(id);
+    }
+    
+    public ArrayList<Alumno> traerAlumnos(){
+        // cremos una lista por medio del metodo
+        List<Alumno> listaDeAlumnos = controlDePersistencia.traerListaAlumnos();
+        // casteamos la lista en un arrayList
+        ArrayList<Alumno> listaAlumnos = new ArrayList<Alumno>(listaDeAlumnos);
+        return  listaAlumnos;
+    }
+}
