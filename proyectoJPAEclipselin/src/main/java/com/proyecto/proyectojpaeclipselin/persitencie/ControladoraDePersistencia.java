4c8e1f709a25f9fc3eb024538d2e1861ab699048
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-12 16:16:24 -0500

    crud con jpa y eclipselin desde NetBeans - creación de entidad materia - implemetación a la clase ControladorDePeristencia - creacion de clase controlador de lógica de la entidad

--- proyectoJPAEclipselin/src/main/java/com/proyecto/proyectojpaeclipselin/persitencie/ControladoraDePersistencia.java
+++ proyectoJPAEclipselin/src/main/java/com/proyecto/proyectojpaeclipselin/persitencie/ControladoraDePersistencia.java
@@ -1,12 +1,16 @@
 package com.proyecto.proyectojpaeclipselin.persitencie;
 
 import com.proyecto.proyectojpaeclipselin.logica.Alumno;
+import com.proyecto.proyectojpaeclipselin.logica.Carrera;
+import com.proyecto.proyectojpaeclipselin.logica.Materia;
 import com.proyecto.proyectojpaeclipselin.persitencie.exceptions.NonexistentEntityException;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ControladoraDePersistencia {
     
+    // instancia de JPAControler de alumno    
     AlumnoJpaController contoladorAlumno = new AlumnoJpaController();
 
     public void crearAlumno(Alumno alumno) {
@@ -30,6 +34,60 @@ public class ControladoraDePersistencia {
     public List<Alumno> traerListaAlumnos(){
         return contoladorAlumno.findAlumnosEntities();
     }
+    
+    
+    // instancia de JPAControler de carrera
+    
+    
+    CarreraJpaController controladorJpaCarrera = new CarreraJpaController();
+
+    public void crearCarrera(Carrera carrera) {
+        controladorJpaCarrera.createCarrera(carrera);
+    }
+
+    public void eliminarCarrera(int id) {
+        controladorJpaCarrera.destroyCarrera(id);
+    }
+
+    public void editarCarrera(Carrera carrera) {
+        controladorJpaCarrera.editCarrera(carrera);
+    }
+
+    public Carrera traerCarrera(int id) {
+        return controladorJpaCarrera.findCarrera(id);
+    }
+
+    public List<Carrera> traerListaCarrera() {
+        return controladorJpaCarrera.findAllCarreras();
+    }
+
+    
+    // instancia de JPAControler de Materia
+    
+    private MateriaJpaController materiaJpaController = new MateriaJpaController();
+    public void crearMateria(Materia materia) {
+        materiaJpaController.create(materia);
+    }
+
+    public void eliminarMateria(int id) {
+        materiaJpaController.destroy(id);
+    }
+
+    public void editarMateria(int id) {
+        materiaJpaController.edit(id);
+    }
+
+    public Materia traerMateria(int id) {
+        return materiaJpaController.findMateria(id);
+    }
+
+    public List<Materia> TraerListaMateria() {
+        
+        return materiaJpaController.findAllMaterias;
+    }
+
+    
+    
     
     
     
