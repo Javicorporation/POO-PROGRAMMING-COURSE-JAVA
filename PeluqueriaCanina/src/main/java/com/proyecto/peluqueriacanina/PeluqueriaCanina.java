12b80d5227ac4e00514df2f64543fb2ae250477f
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-18 21:41:14 -0500

    Proyecto integrador de Java JPA Peluquería canina - errores - mal alineamiento de los nombres en la tabla - mala inicializacion del Entity Manager en el metodo find - no se muestran bien los datos en la tabla

--- PeluqueriaCanina/src/main/java/com/proyecto/peluqueriacanina/PeluqueriaCanina.java
+++ PeluqueriaCanina/src/main/java/com/proyecto/peluqueriacanina/PeluqueriaCanina.java
@@ -7,6 +7,7 @@ public class PeluqueriaCanina {
     public static void main(String[] args) {
         Principal principal = new Principal();
         principal.setVisible(true);
+        
         principal.setLocationRelativeTo(null);
         
     }
