5340eeda8a9beb05237e1a4e491902b6e45340a7
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-25 17:36:43 -0500

    Proyecto K9 escuela canina - Creación de los métodos de crud create, destroy, edit, find   -- el metodo find debe devolver un objeto entidad, mientras que los demas oueden ser 		 void

--- EscuelaK9/src/main/java/com/proyecto/escuelak9/persistencia/OwnerJpaController.java
+++ EscuelaK9/src/main/java/com/proyecto/escuelak9/persistencia/OwnerJpaController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.Persistence;
 
 public class OwnerJpaController implements Serializable {
@@ -56,6 +57,66 @@ public class OwnerJpaController implements Serializable {
             }
         }   
     }
+    
+    
+    //------------ metodo eliminar
+    public void destroy(int id){
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            
+            Owner owner = em.find(Owner.class, id);
+            if (owner == null) {
+                throw new EntityNotFoundException("El Owner con el id "+id+" no existe.");
+            }
+            em.remove(owner);
+            em.getTransaction().commit();
+        } catch (EntityNotFoundException e) {
+            logger.error("Error al eliminar el Owner: {}", e.getMessage(), e);
+            throw e;
+        }finally{
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    //------------ metodo actualizar
+    public void edit(Owner owner){
+        if (owner == null) {
+            throw new IllegalArgumentException("El objeto Owner no puede ser nulo.");        
+        }
+        EntityManager em = null;
+        
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(owner);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Error al actualizar el Owner: {}", e.getMessage(), e);
+            throw new RuntimeException("No se pudo actualizar el owner.", e);
+        }finally{
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    //------------ metodo buscar Owner por Id
+    public Owner find(int id){
+        EntityManager em = null;
+        try {
+            return em.find(Owner.class, id);
+        } finally{
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    //------------ metodo buscar Owner por Id
     
     
     
