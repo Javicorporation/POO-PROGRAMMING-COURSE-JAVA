99e4f5d36c87f0ff7e3526062835599bb2dace51
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-28 15:45:23 -0500

    Proyecto K9 escuela canina - en la clase controlPersistencia se implementan 2 objetos de Jpa de las 2 clases Controller - en la clase controladorLogico se implementa un objeto de la clase control persistencia

---
+++ EscuelaK9/src/main/java/com/proyecto/escuelak9/logica/ControladorLogico.java
@@ -0,0 +1,8 @@
+package com.proyecto.escuelak9.logica;
+
+import com.proyecto.escuelak9.persistencia.ControlPersistencia;
+
+public class ControladorLogico {
+    
+    public ControlPersistencia controlPersistencia = new ControlPersistencia();
+}
