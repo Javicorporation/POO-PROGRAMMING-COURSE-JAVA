7f7f86bb553a2236564579496a7740700da945f3
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-30 14:41:23 -0500

    Proyecto K9 - Implementación de función al botón eliminar - obtenemos el valor de la fila - obtenemos el, id que está en la primera columna de la fila por medio del método ValueOf, getValueAt y getSelectRow de la tabla de la interfaz - el, id lo guardamos en una variable int

--- EscuelaK9/src/main/java/com/proyecto/escuelak9/igu/ViewData.java
+++ EscuelaK9/src/main/java/com/proyecto/escuelak9/igu/ViewData.java
@@ -7,7 +7,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class ViewData extends javax.swing.JFrame {
 
-    ControladorLogico controladorLogico;
+    ControladorLogico controladorLogico = null;
     
     public ViewData() {
         initComponents();
@@ -24,10 +24,10 @@ public class ViewData extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        btnRemove = new javax.swing.JButton();
+        btnEdit = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        TablaDeIntefaz = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -42,13 +42,18 @@ public class ViewData extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
         jLabel3.setText("record information...");
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
-        jButton1.setText("Remove");
+        btnRemove.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        btnRemove.setText("Remove");
+        btnRemove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoveActionPerformed(evt);
+            }
+        });
 
-        jButton2.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
-        jButton2.setText("Edit");
+        btnEdit.setFont(new java.awt.Font("Segoe UI", 3, 14)); // NOI18N
+        btnEdit.setText("Edit");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        TablaDeIntefaz.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {},
                 {},
@@ -59,7 +64,7 @@ public class ViewData extends javax.swing.JFrame {
 
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(TablaDeIntefaz);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -72,9 +77,9 @@ public class ViewData extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnRemove, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(27, 27, 27)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -83,8 +88,8 @@ public class ViewData extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(btnRemove, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 253, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -136,36 +141,52 @@ public class ViewData extends javax.swing.JFrame {
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
         // metodo cargar tabla
         cargarTabla();
+        //controladorLogico.traerMascotasBD();
+        
     }//GEN-LAST:event_formWindowOpened
+
+    private void btnRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveActionPerformed
+        // controlamos que la tabla no este vacia
+        if(TablaDeIntefaz.getRowCount() > 0){
+            // controlamos que haya seleccionado una mascota
+            if (TablaDeIntefaz.getSelectedRow() != -1) {
+                // obtenemos el id de un pet, como esta en String lo pasamos a int 
+                // usamos
+                int idPet = Integer.parseInt(String.valueOf(TablaDeIntefaz.getValueAt(TablaDeIntefaz.getSelectedRow(), 0)));
+                controladorLogico.borrar(idPet);
+                cargarTabla();
+            }else{
+                controladorLogico.mensajeFlotante("No a seleccionado ningun registro", "Advertencia");
+            }
+        }
+    }//GEN-LAST:event_btnRemoveActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTable TablaDeIntefaz;
+    private javax.swing.JButton btnEdit;
+    private javax.swing.JButton btnRemove;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 
     private void cargarTabla() {
-        
-        
-        
+    
         // definimos un modelo para la tabla
         DefaultTableModel tablaDeRegistros = new DefaultTableModel(){
         
             // hacer que los registros no sean editablesç
+            @Override
             public boolean isCellEditable(int row, int column){
                 return false;
-
             }
         };  
         
         // establecemos los nombres de las columnas
-        String titulos[] = {"Name Pet", "Race", "Color", " Allery", "Special", "Name Owner", "address", "Telf"};
+        String titulos[] = {"Id","Name Pet", "Race", "Color", " Allery", "Special", "Name Owner", "address", "Telf"};
         tablaDeRegistros.setColumnIdentifiers(titulos);
         
         // carga de datos desde la base de datos
@@ -174,11 +195,14 @@ public class ViewData extends javax.swing.JFrame {
         // recorremos la lista y lo presentamos en la tabla
         if(ListPet != null){
             for(Pet pet: ListPet){
-            Object[] object = { pet.getNamePet(), pet.getRace(), pet.getColor(), pet.getAllery(), pet.getSpecialTreatment(), 
+                
+            Object[] object = {pet.getId(), pet.getNamePet(), pet.getRace(), pet.getColor(), pet.getAllery(), pet.getSpecialTreatment(), 
                                 pet.getOwner().getNameOwner(), pet.getOwner().getAddress(), pet.getOwner().getTelf()};
-                            tablaDeRegistros.addRow(object);
+            
+            tablaDeRegistros.addRow(object);
             }
             
         }   
+        TablaDeIntefaz.setModel(tablaDeRegistros);
     }
 }
