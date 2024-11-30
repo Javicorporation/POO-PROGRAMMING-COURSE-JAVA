d87f31be065d7e4aeb230cfe20ea1d251de0ee6c
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-11-29 19:33:48 -0500

    Proyecto K9 escuela canina - implementamos funcionalidad al botón exit del Jframe Index, por medio de un mensaje personalizado. - implementamos el método en la controladora lógica y los instancimos en el JFrame 	- en el método salir pasamos  como parámetro un objeto tipo JFrame 	- establecemos una variable entera para las opciones de salir que nos devolverá el método showConfirmDialog de JOptionPane 	- se pasa el Frame, el mensaje, el título, la opción por defecto y un icono 	- por medio de un if verificamos sé la opción fue si

---
+++ EscuelaK9/src/main/java/com/proyecto/escuelak9/EscuelaK9.java
@@ -0,0 +1,12 @@
+package com.proyecto.escuelak9;
+
+import com.proyecto.escuelak9.igu.Index;
+
+public class EscuelaK9 {
+
+    public static void main(String[] args) {
+        Index index = new Index();
+        index.setVisible(true);
+        index.setLocationRelativeTo(null);
+    }
+}
