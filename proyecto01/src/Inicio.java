ae360fb0db09f1977bb86028f1cdeb93cfbfd8a4
Javier Carchipulla S. <krchipullajavier@gmail.com>
2024-12-23 15:57:59 -0500

    funcion de alto nivel o orden - es aquella funcion que recibe como parametro  una funcion y retorna una funcion

--- proyecto01/src/Inicio.java
+++ proyecto01/src/Inicio.java
@@ -1,16 +1,23 @@
 public class Inicio {
-    public static int contador;
-    public static void main(String[] args) {
-        int result = suma(1,2);
-        int result2 = suma(1,2);
-        System.out.println(result +" ,    "+result2);
-        System.out.println(contador);
-    }
-
-
-    // ejemplo de funcion pura
-    public static int suma (int a, int b) {
-        //contador++;
-        return a + b;
-    }
+//    public static int contador;
+//    public static void main(String[] args) {
+//        int result = suma(1,2);
+//        int result2 = suma(1,2);
+//        System.out.println(result +" ,    "+result2);
+//        System.out.println(contador);
+//    }
+//
+//
+//    // ejemplo de funcion pura, solo puede hacer algo especifico
+//    // no debe hacer mas de lo que debe
+//    // no puede tener acciones colaterales
+//    public static int suma (int a, int b) {
+//        //contador++;
+//        return a + b;
+//    }
+//
+//
+//    public static void cadena(String cadena){
+//        return cadena.startsWith("a");
+//    }
 }
