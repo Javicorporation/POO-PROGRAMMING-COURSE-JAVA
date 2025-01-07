2999dc0da90ab78470154522a7158612363da6f9
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-01-07 17:09:37 -0500

    Tipos de funciones - reduce la complejidad de tiempo - añade claridad al momento de escribir y depurar codigo - la recurcividad es muy optima en arboles

--- tipos de funciones/src/Principal.java
+++ tipos de funciones/src/Principal.java
@@ -1,8 +1,17 @@
+import java.util.stream.IntStream;
+
 public class Principal {
     public static void main(String[] args) {
         //System.out.println(sumaSerie(5));
-        funcionCola(5);
-        funcionCabeza(5);
+//        funcionCola(5);
+//        funcionCabeza(5);
+//        System.out.println(sumaSerie(5));
+//        System.out.println(sumaFuncionalRec(5));
+//        System.out.println(sumaRecursival(5));
+
+        System.out.println(factorial(5));
+        System.out.println(factorialRecursival(5));
+        System.out.println(factorialFuncional(5));
 
     }
     // suma de una seria de numero por medio de una funcion iterativa
@@ -15,23 +24,62 @@ public class Principal {
         return resultado;
     }
 
-    //funcion recursiva por cabeza
-    public static void funcionCabeza(int valor){
-        if (valor == 0){
-            return;
-        }
-        funcionCabeza(valor-1);
-        System.out.println(valor);
+    public static int sumaFuncionalRec(int numero){
+        return IntStream.rangeClosed(1,5)
+                .reduce(0, (a, b) -> a + b);
     }
+
+    public static int sumaRecursival(int numero){
+        if (numero == 1){
+            return 1;
+        }
+        return numero + sumaRecursival(numero - 1);
+    }
+
+    // factorial
+    public static int factorial(int numero){
+        int resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado = resultado*i;
+        }
+        return resultado;
+    }
+
+
+    // factorial
+    public static int factorialRecursival(int numero){
+        if (numero == 0){
+            return 1;
+        }
+        return numero * factorialRecursival(numero - 1);
+    }
+
+    public static int factorialFuncional(int numero){
+        return IntStream.rangeClosed(1, numero)
+                .reduce(1, (a, b) -> a*b);
+    }
+
+
+    // factorial
+
+
+    //funcion recursiva por cabeza
+//    public static void funcionCabeza(int valor){
+//        if (valor == 0){
+//            return;
+//        }
+//        funcionCabeza(valor-1);
+//        System.out.println(valor);
+//    }
 
     //funcion recursiva por cola
 
-    public static void funcionCola(int valor){
-        if (valor == 0){
-            return;
-        }
-        System.out.println(valor);
-        funcionCola(valor-1);
-    }
+//    public static void funcionCola(int valor){
+//        if (valor == 0){
+//            return;
+//        }
+//        System.out.println(valor);
+//        funcionCola(valor-1);
+//    }
 
 }
