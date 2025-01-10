660d1082d4f8b3514fe1f45debbce43db78d2216
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-01-09 22:00:17 -0500

    JPA DTO (Data Transfer Object) y JPQL - da error XD

--- DTO y JPA/src/main/java/principio/Principal.java
+++ DTO y JPA/src/main/java/principio/Principal.java
@@ -8,19 +8,29 @@ import java.util.List;
 public class Principal {
 
     public static void main(String[] args) {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadPersonas");
+//        EntityManager em = emf.createEntityManager();
+//        List<Persona> lista=em.createQuery("select p from Persona p", Persona.class).getResultList();
+//
+//        for (Persona p:lista) {
+//
+//            System.out.println(p.getApellidos());
+//            for(Libro l :p.getLibros()) {
+//
+//                System.out.println(l.getTitulo());
+//                System.out.println(l.getPaginas());
+//
+//            }
+//        }
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadPersonas");
         EntityManager em = emf.createEntityManager();
-        List<Persona> lista=em.createQuery("select p from Persona p", Persona.class).getResultList();
+        List<LibroPersonaDTO> lista=em.createQuery("select distinct new com..LibroPersonaDTO( p.apellidos,l.titulo,l.paginas ) from Persona p, Libro l", LibroPersonaDTO.class).getResultList();
+        for (LibroPersonaDTO dto: lista) {
 
-        for (Persona p:lista) {
-
-            System.out.println(p.getApellidos());
-            for(Libro l :p.getLibros()) {
-
-                System.out.println(l.getTitulo());
-                System.out.println(l.getPaginas());
-
-            }
+            System.out.println(dto.getTitulo());
+            System.out.println(dto.getPaginas());
+            System.out.println(dto.getApellidos());
         }
     }
 }
