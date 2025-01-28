529f29a1f13a44785213df007dfcaf8be7ba708f
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-01-27 22:43:45 -0500

    servicio Rest -los servicicos cominican al modelo con el constructor - no olvidarse de las notaciones

--- rest/pruebaRest/src/main/java/com/usar/pruebaRest/controller/JerseyController.java
+++ rest/pruebaRest/src/main/java/com/usar/pruebaRest/controller/JerseyController.java
@@ -1,14 +1,37 @@
 package com.usar.pruebaRest.controller;
 
+import com.usar.pruebaRest.models.Bootcamper;
+import com.usar.pruebaRest.services.BootcamperService;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Path("/")
 public class JerseyController {
-    @GET
-    public String hola(){
-        return "Hola";
+
+    private BootcamperService bootcamperService;
+
+    public JerseyController(BootcamperService bootcamperService) {
+        this.bootcamperService = bootcamperService;
+        this.bootcamperService.add(new Bootcamper("juanin"));
+        this.bootcamperService.add(new Bootcamper("karla"));
+        this.bootcamperService.add(new Bootcamper("maria"));
+        this.bootcamperService.add(new Bootcamper("paz"));
     }
+
+    @GET
+    @Path("/bootcampers")
+    @Produces("/application/json")
+    public List<Bootcamper> listarTodos(){
+        return bootcamperService.getAll();
+    }
+
+    //    @GET
+//    public String hola(){
+//        return "Hola";
+//    }
 }
