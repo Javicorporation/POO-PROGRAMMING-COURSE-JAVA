5bee7e3a30f67434179addf60e82548a5db7fed3
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-02-01 23:55:18 -0500

    Uso del patrón fachada - Facade es un patrón de diseño estructural que proporciona una interfaz simplificada a una biblioteca, un framework o cualquier otro grupo complejo de clases.

---
+++ demo05/src/facade/AudioMixer.java
@@ -0,0 +1,11 @@
+package facade;
+
+import java.io.File;
+
+public class AudioMixer {
+
+    public File fix(VideoFile result){
+        System.out.println("Audio Mixer");
+        return new File("tmp");
+    }
+}
---
+++ demo05/src/facade/BitrateReader.java
@@ -0,0 +1,11 @@
+package facade;
+
+public class BitrateReader {
+
+    public static VideoFile read(VideoFile file, Codec codec) {
+        System.out.println("BitrateReader: reading file");
+            return file;
+    }
+
+    public static VideoFile read(VideoFile file) {}
+}
